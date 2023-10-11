52f16abc581e203a481bca433d9ccf612f00fc2b
devbandeira <dev.bandeira@outlook.com>
2023-10-11 15:50:27 -0300

    Components - Propriedades - CSS MODULE para cada componente

--- ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/App.jsx
+++ ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/App.jsx
@@ -1,10 +1,13 @@
 import Post from "./Post";
+import { Header } from "./components/Header";
 
 function App() {
   return (
   <>
-  <h1>HELLO ALEXANDER</h1>
-  <Post />
+  <Header />
+  <Post name = "Alexander" post = "Novo Poste 1"/>
+  <Post name = "Duda" post = "Nuevo Poste 2"/>
+  <Post name = "Allan" post = " New Poste 3"/>
   </>
   );
 }
--- ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/Post.jsx
+++ ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/Post.jsx
@@ -1,5 +1,12 @@
-function Post(){
-  return <p>POST</p>
+function Post(props){/*Dentro da props eu recebo um OBJETO {} que tem minhas propriedades */
+  return (
+     /*No react quando quero exibir o valor de uma variavel JS dentro do HTML eu uso o {}*/
+    <>
+    <strong>{props.name}</strong>
+    <p>{props.post}</p>
+    </>
+    )
 }
 
 export default Post
+
