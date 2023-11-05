6dccc31e56921117dc756d206bb773a8c52383e8
devbandeira <dev.bandeira@outlook.com>
2023-11-05 16:59:54 -0300

    CSS GLOBAL - CSS MODULE - PADRAO CSS - Hader

--- ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/components/Header.jsx
+++ ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import style from "./Header.module.css";
 
+import igniteLogo from '../assets/ignite-logo.svg';
+
 export function Header (){
   return (
     <header className={style.header}>
     <strong> Alexander Bandeira</strong>
+    <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   )
 }
