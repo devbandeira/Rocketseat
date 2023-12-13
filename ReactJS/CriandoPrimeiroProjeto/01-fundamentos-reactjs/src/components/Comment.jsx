b2477548b8e71a77b02a4b2bba567a0f964dcedd
devbandeira <dev.bandeira@outlook.com>
2023-12-13 12:55:31 -0300

    Criando Componente Comentario - Estrutura HTML

---
+++ ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react';
+
+export function Comment () {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/devbandeira.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Alexander Bandeira</strong>
+              <time title='23 de Janeiro às 10:00' dateTime='2023-01-11'>
+                 cerca de 1 hora atrás
+              </time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20}/>
+            </button>
+          </header>
+
+          <p>Muito bom Alex, parabéns!! </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20}/>
+             Aplaudir <span>20</span>
+          </button>
+        </footer>
+
+      </div>
+    </div>
+  );
+}
--- ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/components/Post.jsx
+++ ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,18 +1,22 @@
-import styles from './Post.module.css';
+import { Comment } from "./Comment";
+import styles from "./Post.module.css";
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/devbandeira.png"/>
+          <img
+            className={styles.avatar}
+            src="https://github.com/devbandeira.png"
+          />
           <div className={styles.authorInfo}>
             <strong>Alexander Bandeira</strong>
             <span>Developer</span>
           </div>
         </div>
 
-        <time title='23 de Janeiro às 10:00' dateTime='2023-01-11'>
+        <time title="23 de Janeiro às 10:00" dateTime="2023-01-11">
           publicado há 1hora
         </time>
       </header>
@@ -21,22 +25,28 @@ export function Post() {
         <p>Primeiro paragrafo</p>
         <p>Segundo paragrafo</p>
         {/* formas de por espaço, como um backspace no REACT {'    '} */}
-        <p><a href=""> #hastag1</a>{' '}
-        <a href=""> #hastag2</a>{' '}
-        <a href=""> #hastag3</a></p>{' '}
+        <p>
+          <a href=""> #hastag1</a> <a href=""> #hastag2</a>{" "}
+          <a href=""> #hastag3</a>
+        </p>{" "}
       </div>
 
       {/*Construção da parte de comentário */}
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
-        <textarea placeholder='Deixe um comentário' 
-        />
+        <textarea placeholder="Deixe um comentário" />
 
         <footer>
-        <button type='submit'>Publicar</button>
+          <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
-  )
+  );
 }
