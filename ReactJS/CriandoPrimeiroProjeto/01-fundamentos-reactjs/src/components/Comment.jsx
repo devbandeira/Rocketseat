ac4619ac72b7df40c913efa3d7e622db5ee83c2c
devbandeira <dev.bandeira@outlook.com>
2024-01-31 18:14:52 -0300

    Realizando likes nos comentarios - Passar somente a funcao para o evento e nunca a execucao

--- ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/components/Comment.jsx
+++ ReactJS/CriandoPrimeiroProjeto/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 export function Comment ({content, onDeleteComment}) {
 
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment(){
     //O ideal seria passar o ID aqui, se tivessemos
     onDeleteComment(content);
+  }
+
+  function handleLikeComment(){
+    setLikeCount(likeCount + 1)
   }
 
   return (
@@ -32,9 +39,9 @@ export function Comment ({content, onDeleteComment}) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp size={20}/>
-             Aplaudir <span>20</span>
+             Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
 
