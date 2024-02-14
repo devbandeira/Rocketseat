95a09846f670afdd8adc8672723291d0e9d0283e
devbandeira <dev.bandeira@outlook.com>
2024-02-13 23:17:10 -0300

    Configurando temas com - Styled Components

--- ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/App.tsx
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/App.tsx
@@ -1,13 +1,18 @@
+// ThemeProvider -> Usado podendo ficar no lugar do FRAGMENT e permitir que passamos
+// passar a propriedade theme={nomeDaConstExportadoPelo default nesse caso} e la
+// no App.tsx conseguir acessar o valor
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 import { Button } from "./components/Button";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
     <Button variant="primary"/>
     <Button variant="secundary"/>
     <Button variant="success"/>
     <Button variant="danger"/>
     <Button />
-    </>
+    </ThemeProvider>
   )
 }
--- ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/components/Button.styles.ts
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/components/Button.styles.ts
@@ -1,24 +1,33 @@
 import styled, { css } from "styled-components";
 
-export type ButtonVariant = 'primary' | 'secundary' | 'danger' | 'success';
+export type ButtonVariant = "primary" | "secundary" | "danger" | "success";
 
 interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
 const buttonVariants = {
-  primary: 'purple',
-  secundary: 'orange',
-  danger: 'red',
-  success: 'green',
-}
+  primary: "purple",
+  secundary: "orange",
+  danger: "red",
+  success: "green",
+};
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
 
-/* Interpolação de STIRNG */
-  ${(props) => {
-    return css`background-color: ${buttonVariants[props.variant]}`
-  }}
-`
+  /* Acessando as propriedades do meu THEMA default.ts */
+  background-color: ${props => props.theme.secondary};
+  color: ${props => props.theme.white}
+
+  /* Interpolação de STIRNG */
+  /* ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `;
+  }} */
+`;
