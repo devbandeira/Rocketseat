13392567c5f230a780a65f20028370ad2c216d60
devbandeira <dev.bandeira@outlook.com>
2024-02-20 17:10:52 -0300

    Configurando o react-router-DOM

--- ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/@types/styled.d.ts
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/@types/styled.d.ts
@@ -1,13 +1,10 @@
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
-type ThemeType = typeof defaultTheme;
-
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
+type ThemeType = typeof defaultTheme
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
-
-
 
 // Descobri o erro -> Na linha 7, faltou o L do DefaultTheme
 // import 'styled-components'
--- ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/App.tsx
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/App.tsx
@@ -1,22 +1,19 @@
 /* ThemeProvider -> Usado podendo ficar no lugar do FRAGMENT e permitir que passamos
 passar a propriedade theme={nomeDaConstExportadoPelo default nesse caso} e la
-no App.tsx conseguir acessar o valor*/
-import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./styles/themes/default";
-import { Button } from "./components/Button";
-import { GlobalStyle } from "./styles/global";
+no App.tsx conseguir acessar o valor */
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-    <GlobalStyle />
-    <Button variant="primary"/>
-    <Button variant="secundary"/>
-    <Button variant="success"/>
-    <Button variant="danger"/>
-    <Button />
-    
-    
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
