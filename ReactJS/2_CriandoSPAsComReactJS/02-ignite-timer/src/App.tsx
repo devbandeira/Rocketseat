12dc28b079bcf81dc405477cc0bab00a83802279
devbandeira <dev.bandeira@outlook.com>
2024-02-22 23:17:48 -0300

    Layout de ROTAS - Componente que se repete em varias rotas

--- ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/App.tsx
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/App.tsx
@@ -1,11 +1,11 @@
 /* ThemeProvider -> Usado podendo ficar no lugar do FRAGMENT e permitir que passamos
 passar a propriedade theme={nomeDaConstExportadoPelo default nesse caso} e la
 no App.tsx conseguir acessar o valor */
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
-import { BrowserRouter } from 'react-router-dom'
-import { Router } from './Router'
 
 export function App() {
   return (
