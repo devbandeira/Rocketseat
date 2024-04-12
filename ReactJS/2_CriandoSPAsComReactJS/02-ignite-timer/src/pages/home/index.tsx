6a3fb47987207b0dc0faad34a373d8f43f8201b2
devbandeira <dev.bandeira@outlook.com>
2024-04-12 16:32:46 -0300

    React Hook Form -> Formas de controlar formulário

--- ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/pages/home/index.tsx
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/pages/home/index.tsx
@@ -1,4 +1,6 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -8,27 +10,53 @@ import {
   StartCountdownButton,
   TaskInput,
 } from './styles'
-import { useState } from 'react'
 
-// uncontrolled -> Buscamos somente a informação do valor INPUT, somente quando precisarmos dela.
 export function Home() {
-  // Assim eu perco a fluides "Habilito algo, desabilito algo", mas ganho em performance.
-  function handleSubmit(event) {
-    // Forma de pegar o valor do input, dando um name="task" para ele e colocando o onSubmit no <form>
-    event.target.task.value
+  // form é um obj que tem várias funções dentro dele que posso usar, posso desestruturar
+  // register -> É um método que vai "add" um input ao nosso formulário, quando uso o useForm()
+  // e o register fala quais são os campos que vou ter no meu formulário. Vou passar la no meu TakInput {...register('darUmNomeParaOInput')}
+  const { register, handleSubmit, watch } = useForm()
+
+  // Importante no dar o nome do método igual o nome do método que foi desestruturado {handleSubmit}
+  // Passando como argumento o data que são os dados de input do nosso formulário
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
   }
+
+  // Habilitando/Desabilitando o btn se o input taks estiver preenchido ou não, e dentro do meu useForm() pego uma fn WATCH
+  // Agora consigo saber o valor do meu campo/input task em tempo real, e la no btn posso usar a estratégia de dizer que se o
+  // o input taks for fazio ou null, vou desabilitar disabled={!task}
+  const task = watch('task')
+  // Criando uma variavel auxiliar que não alteram a performance e nem prejudicam a gente, mas melhora a legibilidade
+  // Ex: const task = watch('task') -> quem le vai ter que correr até o final para entender o que acontecer e pq utilizando isso
+  // Ou seja, o submite desse formulário está desabilitado ou não?(isSubmitDisabled), vai estar desabilitado quando não tenho nada na minha task (!task)
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit} action="">
+      {/* Passando no onSubmit o handleSubmit do useForm() e passar para ele como parametro
+      a nossa função de submit onSubmit={handleSubmit(handleCreateNewCycle)} */}
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
-            id="tasks"
+            id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            /* A fn register recebe um parametro que retorna alguns métodos que retorna alguns métodos que usamos em formulário em JS
+             function register(name: string) {
+                return {
+                  onChange: () => void,
+                  onBlur: () => void,
+                  etc..
+                }
+             }
+             O {...} faz pegar todas as coisas retornadas no regster('task') e acoplha no nosso input como propriedade
+            */
+            {...register('task')}
           />
 
-          {/* Lista de sugestões para o inpu */}
+          {/* Lista de sugestões para o input */}
           <datalist id="task-suggestions">
             <option value="Projeto 1" />
             <option value="Projeto 2" />
@@ -44,6 +72,8 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            // Passando um segundo parâmetro, um objeto de confiurações e podemos passar uma variavel
+            {...(register('minutesAmount'), { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -57,7 +87,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
