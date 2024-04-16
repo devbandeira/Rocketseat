e364243544f3ea29f1f0649c17630125fda226da
devbandeira <dev.bandeira@outlook.com>
2024-04-16 00:38:11 -0300

    Iniciando um novo ciclo - Explicação para ciclos

--- ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/pages/home/index.tsx
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/pages/home/index.tsx
@@ -14,6 +14,7 @@ import {
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import * as zod from 'zod'
+import { useState } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -25,7 +26,29 @@ const newCycleFormValidationSchema = zod.object({
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+// Formato de cada ciclo que eu por dentro da minha aplicação
+// Ter id para identificar os ciclos, pois além do ciclo ativo, tiveram os que ja foram encerrados na pg history
+interface Cycle {
+  id: string
+  task: string
+  minutesAmout: number
+}
+
 export function Home() {
+  // Após o  const newCycle: Cycle =... é necessário eu dizer qual ciclo ta ativo e quais estão desativados, existem duas formas de fazer isso
+  // uma delas seria na interface Cycle eu por um novo atributo isActive: boolean, digo se é TRUE ou FALSE, mas isso trará um problema,
+  // toda vez que eu seta um novo ciclo, tenho que percorrer todo array para ver qual estava ativo e desativar antes do meu novo ciclo ativar.
+  // Ou podemos fazer da seguinte forma
+  // Aqui está iniciado como NULL porque quando eu inicio a aplicação pode ter ID NULL por não ter nenhum ciclo
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  // activeCycleId -> tenho a informação do ciclo ativo
+
+  // É importante definir qual o formato da nossa informação dos ciclos
+  // Aqui vou dizer que ele vai armazenar uma LISTA DE CICLOS <>
+  // REPETINDO DEVE INICIAR O ESTADO COM UMA INFORMAÇÃO DO MESMO TIPO QUE VOU MANUSEAR AO LONGO DA APLICAÇÃO
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  // cycles -> tenho todas informações dos meu ciclos da aplicação
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -34,13 +57,34 @@ export function Home() {
     },
   })
 
-  // Posso observar aqui que o tipo quando passo por cima ainda é any, eu poderia criar uma interface
-  // e deduzir que sei os valores que serão retornados neste objeto, os inputs taks e minutesAmout, mas no useFor()
-  // posso passar os valores padrão para este DATA
+  // Para eu refletir na minha aplicação que um novo ciclo iniciou, eu preciso de um estado. É a unica forma de eu conseguir armazenar
+  // alguma informação do meu componente que vai fazer minha interface reaja a essa informação.
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime())
+    // Criando um novo ciclo aqui dentro
+    // Pondo : Cycle para eu ganhar intelesense do TS
+    const newCycle: Cycle = {
+      // getTime vai pegar a data atual convertida para milisegundos, para criar o ID, para n ter que usar uma lib externa
+      // id: String(new Date().getTime()), tirando aqui de dentro e pondo em uma variavel
+      id,
+      task: data.task,
+      minutesAmout: data.minutesAmount,
+    }
+    // setCycles([...cycles, newCycle]) -------------
+    // REGRA REACT: Toda vez que to alterando o estado e o mesmo, depende da sua versão anterior é legal ele ser
+    // setado em valor de função. (CLOSURES DENTRO DO REACT), então vai ficar:
+    setCycles((state) => [...state, newCycle])
+    // Quando crio um novo ciclo, vou setar o ciclo recem criado como sendo o meu ciclo ativo.
+    // Posso por newCycle.id ou posso criar uma const de chamar de id tirando de dentro do newCycle
+    setActiveCycleId(id)
+
     reset()
   }
+
+  // Para mostrar na tela agora o novo ciclo ativo
+  // Essa variavel vai fazer o seguinte, com base no ID do cliclo ativo, vai percorrer todos CICLOS que tenho e me retornar o ciclo que tem o mesmo id
+  // do ciclo ativo. Ou seja, vai percorrer o vetor de ciclos ativos e encontrar um cyclo em que o ID do ciclo seja igual ao id do ciclo ativo
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   const task = watch('task')
 
