203b646c80d0682d030ebea2885bf48708e528d0
devbandeira <dev.bandeira@outlook.com>
2024-04-29 11:53:42 -0300

    Separando componentes

---
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/pages/home/components/NewCycleForm/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const FormContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+  font-size: 1.125rem;
+  font-weight: bold;
+  /* wrap para quando a tela for menor ele quebrar */
+  flex-wrap: wrap;
+`
+/* Criando um Styled padrao para poder usar em styled-components que compartilham
+o mesmo css, e passando ele com styled(BaseInput) 
+Ou seja, to usando um componente estilizado base, para estilizar outros componentes
+como está detro do mesmo arquivo, não preciso exportar.
+*/
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+
+  color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+`
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
--- ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/pages/home/index.tsx
+++ ReactJS/2_CriandoSPAsComReactJS/02-ignite-timer/src/pages/home/index.tsx
@@ -16,6 +16,10 @@ import {
   StopCountdownButton,
   TaskInput,
 } from './styles'
+
+import { NewCycleForm } from './components/NewCycleForm'
+import { Countdown } from './components/Countdown'
+
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
   minutesAmount: zod
@@ -29,8 +33,8 @@ interface Cycle {
   task: string
   minutesAmount: number
   startDate: Date
-  interruptedDate?: Date // ? Para dizer que ela pode existir ou não caso não interrompida
-  finishedDate?: Date // Para ver a data finalizada e zerar o contador.
+  interruptedDate?: Date
+  finishedDate?: Date
 }
 
 export function Home() {
@@ -58,8 +62,6 @@ export function Home() {
         )
 
         if (secondsDifference >= totalSeconds) {
-          // Quando atualizo um ESTADO setCycles, e seu estado depende do valor dele mesmo anterior, escrevemos isso no formato de função
-          // state => {}
           setCycles((state) =>
             state.map((cycle) => {
               if (cycle.id === activeCycleId) {
@@ -69,10 +71,9 @@ export function Home() {
               }
             }),
           )
-          setAmountSecondsPassed(totalSeconds) // Para poder zerar o contador quando chegar a zero, também poderia passar > no  if (secondsDifference >= totalSeconds)
+          setAmountSecondsPassed(totalSeconds)
           clearInterval(interval)
         } else {
-          // Só vou att a quantidade de segundos se eu não passei da minha quantidade de segundos
           setAmountSecondsPassed(secondsDifference)
         }
       }, 1000)
@@ -98,20 +99,8 @@ export function Home() {
     reset()
   }
 
-  // Fn para interromper o ciclo
-  // chamo essa função no meu btn de interromper
   function handleInterruptCycle() {
-    // Ficou estranho. Mas PRECISAMOS LEMBRAR do principio de imutabilidade do REACT, nunca podemos alterar uma informação sem seguir os principios
-    // da imutabilidade, nesse caso, quando estamos trabalhando com arrays, fica mais chato, pois são arrays de objetos. Então se quero mudar
-    // uma informação de um desses objetos desse arrays de ciclos, obrigatoriamente preciso percorrer todos os itens do array para achar o obj
-    // que quero alterar, ai sim fazer a alteração.
-
-    // Uso aqui o map, porque estou chamando a função "setCycles", estou alterando o valor da variavel que armazena os ciclos da minha aplicação
-    // e preciso dizer qual o novo valor. O Map é importante porque ele vai percorrer cada cilco e vai retornar de dentro do map cada um dos ciclos,
-    // alterados ou não
     setCycles((state) =>
-      // Quando atualizo um ESTADO setCycles, e seu estado depende do valor dele mesmo anterior, escrevemos isso no formato de função
-      // state => {}
       state.map((cycle) => {
         if (cycle.id === activeCycleId) {
           return { ...cycle, interruptedDate: new Date() }
@@ -130,8 +119,6 @@ export function Home() {
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
 
-  // Toda vez que meus minutes ou seconds atualizarem eu quero mudar o titlo da minha janela
-  // para uma stirng contendo minutes e segundos.
   useEffect(() => {
     if (activeCycle) {
       document.title = `${minutes}:${seconds}`
@@ -143,42 +130,8 @@ export function Home() {
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)}>
-        <FormContainer>
-          <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput
-            id="task"
-            list="task-suggestions"
-            placeholder="Dê um nome para o seu projeto"
-            disabled={!!activeCycle} // Aqui precisa ser um boolean, então uso !! para se tiver algum valor dentro converter para true, se não para false
-            {...register('task')}
-          />
-          <datalist id="task-suggestions">
-            <option value="Projeto 1" />
-            <option value="Projeto 2" />
-            <option value="Projeto 3" />
-            <option value="Banana" />
-          </datalist>
-          <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput
-            type="number"
-            id="minutesAmount"
-            placeholder="00"
-            step={5}
-            min={1}
-            max={60}
-            disabled={!!activeCycle}
-            {...register('minutesAmount', { valueAsNumber: true })}
-          />
-          <span>minutos.</span>
-        </FormContainer>
-        <CountdownContainer>
-          <span>{minutes[0]}</span>
-          <span>{minutes[1]}</span>
-          <Separator>:</Separator>
-          <span>{seconds[0]}</span>
-          <span>{seconds[1]}</span>
-        </CountdownContainer>
-
+        <NewCycleForm />
+        <Countdown />
         {activeCycle ? (
           <StopCountdownButton onClick={handleInterruptCycle} type="button">
             <HandPalm size={24} />
